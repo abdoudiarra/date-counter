e5669cdc869ec90e100c0db06bbf8662fab8785f
abdoudiarra <58839578+abdoudiarra@users.noreply.github.com>
2023-08-26 17:46:04 -0400

    practice of useState and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  //current date
+  //current date will also change state since it shows the next or previous date
+  const [date, manageDate] = useState(new Date());
+  //declare state of step
+  const [step, manageStep] = useState(1);
+  //declare state of count (count * step)
+  const [count, manageCount] = useState(0);
+
+  //function that adds step
+  function addStep(){
+    manageStep((currentStep)=>currentStep + 1);
+  }
+  //function that removes step
+  function removeStep(){
+    manageStep((currentStep)=>currentStep - 1);
+  }
+  //function that adds Count
+  function addCount(){
+    manageCount((currentCount)=>currentCount+1*step);
+    date.setDate(date.getDate() + step);
+    manageDate(date);
+    
+  }
+  //function that removes Count
+  function removeCount(){
+    manageCount((currentCount)=>currentCount-1*step);
+    date.setDate(date.getDate() - step);
+    manageDate(date);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='step'>
+        <button onClick={removeStep}>-</button>
+        <span>Step:{step}</span>
+        <button onClick={addStep}>+</button>
+      </div>
+      <div className='counter'>
+        <button onClick={removeCount}>-</button>
+        <span>Count:{count}</span>
+        <button onClick={addCount}>+</button>
+      </div>
+      <div className='date'>
+        {
+          count == 0 ? <p>Today is {date.toDateString()}</p> : ""
+        }
+        {
+          count > 0 ? <p>{count} days from today is {date.toDateString()}</p> : ""
+        }
+        {
+          count < 0 ? <p>{count} days ago was {date.toDateString()}</p> : ""
+        }
+      </div>
     </div>
   );
 }
